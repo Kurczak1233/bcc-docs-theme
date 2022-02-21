9d99e29669552a9beb574f6ea71a5d8d76d01dd0
Nicolai Velzel <nicolai.velzel@gmail.com>
2022-02-21 09:50:48 +0100

    add html search functionality

--- _layouts/scripts/search.ts
+++
@@ -1,8 +0,0 @@
-﻿export async function generateSearchUrl(searchCode: string) {
-
-    const searchUrl: string = `https://github.com/search?q=org%3Abcc-code+${searchCode.toString()}`
-    console.log(searchUrl);
-
-    // window.location.href = searchUrl
-
-}
