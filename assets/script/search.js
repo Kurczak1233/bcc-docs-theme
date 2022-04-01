7b5af855a2a8ac861e9ff51985d5b6a2265214d7
Nicolai Velzel <nicolai.velzel@gmail.com>
2022-04-01 15:46:43 +0200

    update search

--- assets/script/search.js
+++ assets/script/search.js
@@ -21,8 +21,7 @@ async function searchInFiles() {
     const searchInput = document.getElementById("search_input");
     const searchInputValue = searchInput.value;
 
-    // const searchUrl = `https://api.github.com/search/code?q=${searchInputValue}+in:file+language:markdown+repo:bcc-code/bcc-code.github.io`;
-    const searchUrl = `https://api.github.com/search/code?q=${searchInputValue}+in:file+language:markdown+org:bcc-code`;
+    const searchUrl = `https://api.github.com/search/code?q=${searchInputValue}+in:file+language:markdown+org:bcc-code&per_page=50`;
 
     const getMarkdownData = await fetch(searchUrl).then(response => response.json()).catch(error => console.log(error));
     console.log(getMarkdownData);
@@ -33,172 +32,53 @@ async function searchInFiles() {
         const markdownRepositoryName = markdownRepositoryData.name;
         const markdownFileName = data.name;
         const markdownFilePath = data.path;
+        const markdownHtmlUrl = data.html_url;
+        const markdownRepo = data.repository;
+        const markdownRepoUrl = markdownRepo.html_url;
 
         const sliceFilePath = markdownFilePath.slice(0, -3);
 
-        // const markdownToHtml = `/${markdownRepositoryName}/${sliceFilePath}`;
-        const markdownPageUrl = `/${sliceFilePath}`;
-        // let markdownPageUrl;
-        // markdownPageUrl = String(markdownToHtml).replace(/^bcc-code\.github\.io/, '');
+
+        let markdownPageUrl = String(sliceFilePath);
+        const findDocs = 'docs';
+        const foundDocs = markdownPageUrl.match(findDocs);
+        const findRepo = 'docs';
+        const foundRepo = markdownPageUrl.match(findRepo);
+
+        if (foundDocs !== null && foundDocs[0] === findDocs) {
+
+            markdownPageUrl = String(sliceFilePath).replace(/^docs/, markdownRepositoryName);
+
+        } else if (foundRepo !== null && foundRepo[0] === findRepo) {
+
+            markdownPageUrl = String(sliceFilePath).replace(/^bcc-code.github.io/, "");
+
+        } else {
+
+            markdownPageUrl = markdownHtmlUrl
+
+        }
+
 
         const createLocation = document.getElementById("search_results")
         let createListItem = document.createElement("li");
         let searchSuggestion = document.createElement("a");
+        let repoSpan = document.createElement("a")
 
         createListItem.className = "search_suggestion";
 
         searchSuggestion.setAttribute("href", `${markdownPageUrl}`);
-        searchSuggestion.innerText = `${markdownFileName}`;
+        searchSuggestion.innerText = `${sliceFilePath}`;
+
+        repoSpan.className = "result_repository"
+        repoSpan.setAttribute("href", `${markdownRepoUrl}`);
+        repoSpan.setAttribute("target", '_blank');
+        repoSpan.innerText = `${markdownRepositoryName}`;
 
         createLocation.appendChild(createListItem).appendChild(searchSuggestion);
+        createLocation.appendChild(createListItem).appendChild(repoSpan);
 
 
     }
 
 }
-
-// function autoComplete() {
-//     let input, filter, ul, li, a, i;
-//     input = document.getElementById("search_code_input");
-//     filter = input.value.toUpperCase();
-//     ul = document.getElementById("search_suggestions");
-//     li = ul.getElementsByTagName("li");
-//
-//     for (i = 0; i < li.length; i++) {
-//
-//         a = li[i].getElementsByTagName("a")[0];
-//         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-//             li[i].style.display = "";
-//         } else {
-//             li[i].style.display = "none";
-//         }
-//     }
-// }
-//
-// function searchOnClickFunction() {
-//
-//     const searchCode = document.getElementById("search_code_input").value;
-//     window.location.href = "https://github.com/search?q=org%3Abcc-code+" + searchCode;
-//
-// }
-//
-// async function searchInFiles() {
-//
-//     const searchCode = document.getElementById("search_code_input").value;
-//     const searchUrl = `https://api.github.com/search/code?q=${searchCode}+in:file+language:markdown+org:bcc-code&per_page=100&page=1`;
-//
-//     const getMarkdownData = await fetch(searchUrl).then(response => response.json()).catch(error => console.log(error));
-//
-//
-//     console.log(getMarkdownData);
-//
-// }
-//
-//
-// async function listMarkdownFiles() {
-//
-//     let newElementsLocation = document.getElementById("search_suggestions");
-//
-//     if (sessionStorage.length !== 0) {
-//         console.log("There is data in the session!");
-//
-//         for (let searchNumber = 1; searchNumber <= sessionStorage.length; searchNumber++) {
-//
-//             let data = sessionStorage[searchNumber];
-//
-//             data = JSON.parse(data)
-//
-//             let firstKey = Object.keys(data)[0];
-//             let secondKey = Object.keys(data)[1];
-//             let thirdKey = Object.keys(data)[2];
-//
-//             let markdownRepositoryName = data[firstKey];
-//             let markdownFileName = data[secondKey];
-//             let markdownFilePath = data[thirdKey];
-//
-//             const sliceFilePath = markdownFilePath.slice(0, -3);
-//
-//             const markdownPageUrl = `/${markdownRepositoryName}/${sliceFilePath}.html`;
-//
-//             let createListItem = document.createElement("li");
-//             let searchSuggestion = document.createElement("a");
-//             let searchSuggestionRepository = document.createElement("span");
-//
-//             createListItem.className = "search_suggestion";
-//
-//             searchSuggestion.setAttribute("href", `${markdownPageUrl}`);
-//             searchSuggestion.innerText = `${markdownFileName}`;
-//
-//             searchSuggestionRepository.className = "search_suggestion";
-//             searchSuggestionRepository.innerText = `${markdownRepositoryName}`;
-//
-//             newElementsLocation.appendChild(createListItem).appendChild(searchSuggestion);
-//             newElementsLocation.appendChild(createListItem).appendChild(searchSuggestion).appendChild(searchSuggestionRepository);
-//
-//
-//         }
-//
-//     } else {
-//         console.log("The session data is empty... adding data")
-//
-//         let getMarkdownFilesUrl;
-//
-//         getMarkdownFilesUrl = [
-//             `https://api.github.com/search/code?q=language:markdown+org:bcc-code&per_page=100&page=1`,
-//             `https://api.github.com/search/code?q=language:markdown+org:bcc-code&per_page=100&page=2`,
-//             `https://api.github.com/search/code?q=language:markdown+org:bcc-code&per_page=100&page=3`,
-//             `https://api.github.com/search/code?q=language:markdown+org:bcc-code&per_page=100&page=4`
-//         ];
-//
-//         let data;
-//         let urlNumber = 1;
-//
-//         for (const url of getMarkdownFilesUrl) {
-//
-//             const getMarkdownData = await fetch(url).then(response => response.json()).catch(error => console.log(error));
-//             for (data of getMarkdownData.items) {
-//
-//                 const markdownRepositoryData = data.repository;
-//                 const markdownRepositoryName = markdownRepositoryData.name;
-//                 const markdownFileName = data.name;
-//                 const markdownFilePath = data.path;
-//
-//                 const saveToSession = {
-//                     markdownRepositoryName,
-//                     markdownFileName,
-//                     markdownFilePath
-//                 }
-//
-//                 sessionStorage.setItem(urlNumber, JSON.stringify(saveToSession));
-//                 const sliceFilePath = markdownFilePath.slice(0, -3);
-//
-//                 const markdownPageUrl = `/${markdownRepositoryName}/${sliceFilePath}.html`;
-//
-//                 let createListItem = document.createElement("li");
-//                 let searchSuggestion = document.createElement("a");
-//                 let searchSuggestionRepository = document.createElement("span");
-//
-//                 createListItem.className = "search_suggestion";
-//
-//                 searchSuggestion.setAttribute("href", `${markdownPageUrl}`);
-//                 searchSuggestion.innerText = `${markdownFileName}`;
-//
-//                 searchSuggestionRepository.className = "search_suggestion";
-//                 searchSuggestionRepository.innerText = `${markdownRepositoryName}`;
-//
-//                 newElementsLocation.appendChild(createListItem).appendChild(searchSuggestion);
-//                 newElementsLocation.appendChild(createListItem).appendChild(searchSuggestion).appendChild(searchSuggestionRepository);
-//
-//                 urlNumber++
-//             }
-//
-//         }
-//
-//         console.log("Data added")
-//
-//     }
-//
-//
-// }
-//
-// listMarkdownFiles();
